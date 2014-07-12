662344e73cc6b24910ba08ad8154d3e6b1bdf966
huangz1990 <huangz1990@gmail.com>
2014-07-12 16:03:47 +0800

    添加指向 RedisDoc.com 文档引用

--- conf.py
+++ conf.py
@@ -110,7 +110,7 @@ html_theme = 'default'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = project
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
@@ -256,3 +256,8 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+intersphinx_mapping = { 'redis': ('http://www.redisdoc.com/en/latest/', None) }
+intersphinx_cache_limit = 1
+
+
